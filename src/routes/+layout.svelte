7b3fa94145b931add5b1ec74afd10645ab24dffd
agol <andre.goldmann@hacon.de>
2023-08-22 14:35:48 +0200

    Initial Setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,50 +6,19 @@
 </script>
 <pre>{ JSON.stringify(data)}</pre>
 <div class="app">
-	<Header />
+	<button class="btn">Button</button>
+	<button class="btn btn-neutral">Neutral</button>
+	<!--Header />
 
 	<main>
 		<slot />
-	</main>
+	</main-->
 
-	<footer>
+	<!--footer>
 		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+	</footer-->
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
 </style>
