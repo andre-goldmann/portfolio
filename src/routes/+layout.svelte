6a170a8a8abd4da79bf5db0512218d5cd084f66e
agol <andre.goldmann@hacon.de>
2023-08-22 13:16:17 +0200

    Initial Setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,10 @@
 <script>
-	import { browser } from '$app/environment';
-	import { page } from '$app/stores';
-	import { webVitals } from '$lib/vitals';
 	import Header from './Header.svelte';
 	import './styles.css';
 
-	/** @type {import('./$types').LayoutServerData} */
 	export let data;
-
-	$: if (browser && data?.analyticsId) {
-		webVitals({
-			path: $page.url.pathname,
-			params: $page.params,
-			analyticsId: data.analyticsId
-		});
-	}
 </script>
-
+<pre>{ JSON.stringify(data)}</pre>
 <div class="app">
 	<Header />
 
