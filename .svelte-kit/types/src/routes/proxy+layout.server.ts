ad2d52bfd6867d6e8d582e058345a954803e169a
agol <andre.goldmann@hacon.de>
2023-08-22 14:05:59 +0200

    Initial Setup

---
+++ .svelte-kit/types/src/routes/proxy+layout.server.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import type { LayoutServerLoad } from './$types';
+
+export const load = async () => {
+    return {
+        posts: ["1", "2"],
+    };
+};;null as any as LayoutServerLoad;
