959685ca69872b9650a12cc0997036ea325d4468
agol <andre.goldmann@hacon.de>
2023-08-31 20:40:51 +0200

    initial website setup

--- src/lib/orderflow.worker.ts
+++ src/lib/orderflow.worker.ts
@@ -1,19 +1,19 @@
-import type {PostMessage, PostMessageDataRequest} from "./post-message";
-
-onmessage = ({data: {msg}}:
-MessageEvent<PostMessage<PostMessageDataRequest>>) => {
-    switch (msg) {
-        case 'start':
-            // orderFlowStoreJob.start();
-            // orderFlowDeleteJob.start();
-            break;
-        case 'stop':
-            console.info("Stop jobs");
-    }
-};
-
-const startJobs = () => {
-    console.info("Starting jobs");
-};
-
-export {};
+// import type {PostMessage, PostMessageDataRequest} from "./post-message";
+//
+// onmessage = ({data: {msg}}:
+// MessageEvent<PostMessage<PostMessageDataRequest>>) => {
+//     switch (msg) {
+//         case 'start':
+//             // orderFlowStoreJob.start();
+//             // orderFlowDeleteJob.start();
+//             break;
+//         case 'stop':
+//             console.info("Stop jobs");
+//     }
+// };
+//
+// const startJobs = () => {
+//     console.info("Starting jobs");
+// };
+//
+// export {};
